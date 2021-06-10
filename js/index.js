24349a614d339bfdbdd7d84bd48f2d500beb1469
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2021-06-10 13:16:24 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 alert('it works!');
 console.log("it works!!");
 document.body.style.backgroundColor="green";
+window.requestAnimationFrame(function loop(t) {
+    //console.log(t);
+    document.getElementById("countup-browser-refresh").innerText=Math.floor(t/1000);
+    window.requestAnimationFrame(loop);
+});
