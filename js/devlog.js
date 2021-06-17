af9002e28690ddd861b264b7e4c5c1bbade235b4
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2021-06-17 13:04:23 -0500

    Update devlog.js

--- js/devlog.js
+++ js/devlog.js
@@ -1,6 +1,6 @@
 window.devlog = [{
 	date: new Date("2021-06-17"),
-	text: "Created developer log",
+	text: "Created developer log \n created canvas",
 }, {
 	date: new Date("2021-06-10"),
 	text: "Created count up timer \n changed colors \n request animation frame",
