fc52bb02d14675c948ec836f08e8fd1ab5f1a87c
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2021-06-17 12:43:30 -0500

    Create devlog.js

---
+++ js/devlog.js
@@ -0,0 +1,10 @@
+window.devlog = [{
+	date: new Date("2021-06-17"),
+	text: "Created developer log",
+}, {
+	date: new Date("2021-06-10"),
+	text: "Created count up timer \n changed colors \n request animation frame",
+}, {
+	date: new Date("2021-06-03"),
+	text: "Created Github page \n I know nothing \n Bit knows all",
+}];
