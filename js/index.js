d766fa5fd505774b23a54807224750c59c271eaa
Jesse Robertson <jesse.d.robertson@gmail.com>
2021-07-09 13:51:37 -0500

    Add Basic Canvas Drawing and Physics

--- js/index.js
+++ js/index.js
@@ -1,17 +1,102 @@
-alert(JSON.stringify(devlog,undefined,4));
+// alert(JSON.stringify(devlog, undefined, 4));
 console.log(devlog);
-document.body.style.backgroundColor="green";
-window.requestAnimationFrame(function loop(t) {
-    //console.log(t);
-    document.getElementById("countup-browser-refresh").innerText=Math.floor(t/1000);
-    window.requestAnimationFrame(loop);
-});
+// document.body.style.backgroundColor = "green";
+
 const canvas = document.getElementById('canvasTest');
-canvas.width = window.innerWidth;
 const ctx = canvas.getContext('2d');
 
-const width = window.innerWidth;
+window.addEventListener('resize', positionCanvas);
+window.addEventListener('load', positionCanvas);
 
-ctx.fillStyle = 'blue';
-ctx.fillRect(0, 0, width, 100); //pixels, border, border, width, height
-//eventually create smart; have it find screen size and display full screen
+function positionCanvas() {
+	canvas.style.position = 'fixed';
+	canvas.style.top = 0;
+	canvas.style.left = 0;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
+
+let lastTime;
+window.requestAnimationFrame(function loop(t) {
+	if (lastTime) {
+		const dt = (t - lastTime) / 1000;
+		update(dt);
+	}
+
+	lastTime = t;
+	window.requestAnimationFrame(loop);
+});
+
+setInterval(() => {
+	/* do stuff */
+	console.log('this is the minute interval thing')
+}, 60 * 1000)
+
+
+const bouncingSquare = {
+	size: 50,
+	position: {
+		x: 100,
+		y: 100,
+	},
+	velocity: { // pixels per second
+		x: 300,
+		y: -200,
+	},
+};
+
+
+function updateAndDrawBouncingBall(dt) {
+	const s = bouncingSquare;
+
+	// gravity
+	s.velocity.y += 300 * dt
+
+	// update position from velocity
+	s.position.x += s.velocity.x * dt;
+	s.position.y += s.velocity.y * dt;
+
+	// bounce
+	if ((s.position.x + s.size) > canvas.width) {
+		s.velocity.x *= -1
+	}
+
+	if ((s.position.y + s.size) > canvas.height) {
+		s.velocity.y *= -1
+	}
+
+	if (s.position.x < 0) {
+		s.velocity.x *= -1
+	}
+
+	if (s.position.y < 0) {
+		s.velocity.y *= -1
+	}
+
+	// draw
+	ctx.fillStyle = 'blue';
+	ctx.fillRect(s.position.x, s.position.y, s.size, s.size);
+}
+
+// todo: break up update vs draw
+function update(dt) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	updateAndDrawBouncingBall(dt);
+
+	drawFrameRateIndicator(dt);
+
+	document
+		.getElementById("countup-browser-refresh")
+		.innerText = Math.floor(lastTime / 1000);
+}
+
+
+function drawFrameRateIndicator(dt) {
+	const formattedFrameRate = (1 / dt).toFixed(4);
+
+	const fontSize = 20;
+	ctx.fillStyle = 'gray';
+	ctx.font = `${fontSize}px Arial`;
+	ctx.fillText(formattedFrameRate, 0, fontSize);
+}
