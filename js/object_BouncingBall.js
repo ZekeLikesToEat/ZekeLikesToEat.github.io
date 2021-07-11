0115791d7baaab5ff10eea62a3da36d8b180da03
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2021-07-11 13:32:43 -0500

    Create object_BouncingBall.js

---
+++ js/object_BouncingBall.js
@@ -0,0 +1,43 @@
+const bouncingSquare = {
+	size: 50,
+	position: {
+		x: 100,
+		y: 100,
+	},
+	velocity: { // pixels per second
+		x: 300,
+		y: -200,
+	},
+};
+
+function updateAndDrawBouncingBall(dt) {
+	const s = bouncingSquare;
+
+	// gravity
+	s.velocity.y += 300 * dt
+
+	// update position from velocity
+	s.position.x += s.velocity.x * dt;
+	s.position.y += s.velocity.y * dt;
+
+	// bounce
+	if ((s.position.x + s.size) > canvas.width) {
+		s.velocity.x *= -1
+	}
+
+	if ((s.position.y + s.size) > canvas.height) {
+		s.velocity.y *= -1
+	}
+
+	if (s.position.x < 0) {
+		s.velocity.x *= -1
+	}
+
+	if (s.position.y < 0) {
+		s.velocity.y *= -1
+	}
+
+	// draw
+	ctx.fillStyle = 'blue';
+	ctx.fillRect(s.position.x, s.position.y, s.size, s.size);
+}
