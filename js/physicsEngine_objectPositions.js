fc06bad804ca68992082876e68556023d981da8f
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2021-07-12 13:08:30 -0500

    Update physicsEngine_objectPositions.js

--- js/physicsEngine_objectPositions.js
+++ js/physicsEngine_objectPositions.js
@@ -1,7 +1,7 @@
 //gather all positions here; for now, display positions.
 const objectPositionsList =[];
 
-function declareObjectPositions {
+function declareObjectPositions() {
   var arrayLength = objectPositionsList.length;
   for (var i = 0; i < arrayLength; i++) {
     console.log(myStringArray[i]);
