5c45f84a565ccbcad12394d7169ba2e3e3655e2e
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2021-07-12 12:53:13 -0500

    Create physicsEngine_objectPositions.js

---
+++ js/physicsEngine_objectPositions.js
@@ -0,0 +1,9 @@
+//gather all positions here; for now, display positions.
+const objectPositionsList =[];
+
+function declareObjectPositions {
+  var arrayLength = objectPositionsList.length;
+  for (var i = 0; i < arrayLength; i++) {
+    console.log(myStringArray[i]);
+  }
+}
