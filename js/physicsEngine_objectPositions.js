6b1c5c51b450c8f9370fe8cc8a00670c75911c9b
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2021-07-13 12:17:34 -0500

    Update physicsEngine_objectPositions.js

--- js/physicsEngine_objectPositions.js
+++ js/physicsEngine_objectPositions.js
@@ -4,12 +4,19 @@ const objectPositionsList =[];
 function declareObjectPositions() {
   var arrayLength = objectPositionsList.length;
   for (var i = 0; i < arrayLength; i++) {
-    console.log(objectPositionsList[i]);
-    //var rect = i.getBoundingClientRect();
-//console.log(rect.top, rect.right, rect.bottom, rect.left);
     console.log("attempting to show coordinates");
-    console.log(i.position());
-    console.log(i.position(x));
-    console.log(i.position(y));
+    
+    //console.log(objectPositionsList[i]);
+    console.log(objectPositionsList[i].position);
+    console.log(objectPositionsList[i].position.y);
+    console.log(objectPositionsList[i].position.y);
+    
+    /*FAILURES:
+      //var rect = i.getBoundingClientRect();
+      //console.log(rect.top, rect.right, rect.bottom, rect.left);
+      console.log(i.position());
+      console.log(i.position(x));
+      console.log(i.position(y));
+    */
   }
 }
