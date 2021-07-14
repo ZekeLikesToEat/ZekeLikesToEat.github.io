37386af377573c125cc8ee50ae8de1e05a143dcc
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2021-07-14 13:15:35 -0500

    Update physicsEngine_objectPositions.js

--- js/physicsEngine_objectPositions.js
+++ js/physicsEngine_objectPositions.js
@@ -4,12 +4,39 @@ const objectPositionsList =[];
 function declareObjectPositions() {
   var arrayLength = objectPositionsList.length;
   for (var i = 0; i < arrayLength; i++) {
-    
-    console.log(objectPositionsList[i].objectName + ", X: " 
-                +objectPositionsList[i].position.x.toFixed(2) + ", Y: " 
-                + objectPositionsList[i].position.y.toFixed(2));
-    
-    
+    var ob = objectPositionsList[i];
+    console.log(ob.objectName + ", X: " 
+                +ob.position.x.toFixed(2) + ", Y: " 
+                +ob.position.y.toFixed(2));
+    isThereACollision(ob);
+  }
+  objectPositionsList.length =0;
+}
+ 
+function isThereACollision(var ob) {
+   var arrayLength = objectPositionsList.length;
+  for (var i = 0; i < arrayLength; i++) {
+    if(objectPositionsList[i] = ob) return;
+    if(Math.abs(ob.position.x - objectPositionsList[i].x)< objectPositionsList[i].size) {
+       console.log(ob.objectName + " collided with " +  
+       objectPositionsList[i].objectName + " on the X axis at : " +
+       ob.position.x.toFixed(2) + " = " +
+       objectPositionsList[i].position.x.toFixed(2));   
+    }
+    if(Math.abs(ob.position.y - objectPositionsList[i].y)< objectPositionsList[i].size) {
+       console.log(ob.objectName + " collided with " +  
+       objectPositionsList[i].objectName + " on the y axis at : " +
+       ob.position.x.toFixed(2) + " = " +
+       objectPositionsList[i].position.x.toFixed(2));   
+    }
+    if((Math.abs(ob.position.x - objectPositionsList[i].x)> objectPositionsList[i].size) &&
+      (Math.abs(ob.position.y - objectPositionsList[i].y)< objectPositionsList[i].size))
+    {
+      console.log("No collision");
+    }
+  }
+}
+
     /*SUCCESS
     console.log("attempting to show coordinates");
     console.log(objectPositionsList[i]);
@@ -25,6 +52,3 @@ function declareObjectPositions() {
       console.log(i.position(x));
       console.log(i.position(y));
     */
-  }
-  objectPositionsList.length =0;
-}
