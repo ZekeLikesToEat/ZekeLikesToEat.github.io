f04369c1b80f81495c3d205adcabe4791498f051
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2022-03-05 07:24:50 -0600

    Create updatesystem.js

---
+++ js/updatesystem.js
@@ -0,0 +1,34 @@
+//limiting the access to only this script unless explicitly called upon
+const FrameRateSystem = (function () {
+	const module = {};
+ //place script here
+  
+  /*function update(dt) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	FrameRateSystem.drawFrameRateIndicator(dt,ctx);
+
+	document
+		.getElementById("countup-browser-refresh")
+		.innerText = Math.floor(lastTime / 1000);
+}*/
+  
+  
+  
+  
+  
+	/*module.drawFrameRateIndicator = function(dt,ctx) {
+		const formattedFrameRate = (1 / dt).toFixed(4);
+		const fontSize = 20;
+		ctx.fillStyle = 'gray';
+		ctx.font = `${fontSize}px Arial`;
+		ctx.fillText(formattedFrameRate, 0, fontSize);
+	}*/
+  
+  
+  
+  
+	// end script
+	return module;
+})()
+
+//Class -->Objects-->functions (but you can have classes objects [they have no class.. shame])
