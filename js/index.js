1d654d67a1375ec2944b86543ee79c8a5776a10d
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2022-03-04 21:27:18 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,51 @@
+//New Code 2022.03.04
+const canvas = document.getElementById('canvasTest');
+const ctx = canvas.getContext('2d');
+window.addEventListener('resize', positionCanvas);
+window.addEventListener('load', positionCanvas);
+function positionCanvas() {
+	canvas.style.position = 'fixed';
+	canvas.style.top = 0;
+	canvas.style.left = 0;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
+let lastTime;
+window.requestAnimationFrame(function loop(t) {
+	if (lastTime) {
+		const dt = (t - lastTime) / 1000;
+		update(dt);
+	}
+	lastTime = t;
+	window.requestAnimationFrame(loop);
+});
+setInterval(() => {
+	// do stuff 
+	
+}, 60 * 1000/120)//changed to every 1/2 seconds
+function update(dt) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawFrameRateIndicator(dt);
+
+	document
+		.getElementById("countup-browser-refresh")
+		.innerText = Math.floor(lastTime / 1000);
+}
+/* Move this to separate script
+function drawFrameRateIndicator(dt) {
+	const formattedFrameRate = (1 / dt).toFixed(4);
+
+	const fontSize = 20;
+	ctx.fillStyle = 'gray';
+	ctx.font = `${fontSize}px Arial`;
+	ctx.fillText(formattedFrameRate, 0, fontSize);
+}*/
+
+
+
+/*
+//Old Code  2022.03.04
+
 // alert(JSON.stringify(devlog, undefined, 4));
 console.log(devlog);
 // document.body.style.backgroundColor = "green";
@@ -28,7 +76,7 @@ window.requestAnimationFrame(function loop(t) {
 });
 
 setInterval(() => {
-	/* do stuff */
+	//do stuff 
 	//console.log('this is the minute interval thing')
 	addObjectTo_objectPositionsList();
 	addObjectTo_objectPositionsList2();//delete after testing
@@ -59,4 +107,4 @@ function drawFrameRateIndicator(dt) {
 	ctx.fillStyle = 'gray';
 	ctx.font = `${fontSize}px Arial`;
 	ctx.fillText(formattedFrameRate, 0, fontSize);
-}
+}*/
