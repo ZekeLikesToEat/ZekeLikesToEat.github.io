2516b98c386d649e0a4b2c63321d2e340e7ac367
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2022-03-04 22:04:01 -0600

    Update sys_p_framrate_script.js

--- js/sys_p_framrate_script.js
+++ js/sys_p_framrate_script.js
@@ -1,11 +1,16 @@
 //limiting the access to only this script unless explicitly called upon
-const sys_P_FrameRate - (function () {
+const FrameRateSystem = (function () {
+	const module = {};
  //place script here
-function drawFrameRateIndicator(dt) {
-	const formattedFrameRate = (1 / dt).toFixed(4);
-	const fontSize = 20;
-	ctx.fillStyle = 'gray';
-	ctx.font = `${fontSize}px Arial`;
-	ctx.fillText(formattedFrameRate, 0, fontSize);
+	module.drawFrameRateIndicator = function(dt,ctx) {
+		const formattedFrameRate = (1 / dt).toFixed(4);
+		const fontSize = 20;
+		ctx.fillStyle = 'gray';
+		ctx.font = `${fontSize}px Arial`;
+		ctx.fillText(formattedFrameRate, 0, fontSize);
+	}
+	// end script
+	return module;
+})()
 
-} // end script
+//Class -->Objects-->functions (but you can have classes objects [they have no class.. shame])
