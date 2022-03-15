7d00325cfa80ffb2e8f8d2a5832bdb1aca970b91
ZekeLikesToEat <80850078+ZekeLikesToEat@users.noreply.github.com>
2022-03-15 15:49:56 -0500

    Update and rename system_Factory_BouncingBall.js to factorynewball.js

--- js/factorynewball.js
+++ js/factorynewball.js
@@ -1,7 +1,7 @@
 //Creating factory to create object "Bouncing Balls"
 
 //limiting the access to only this script unless explicitly called upon
-const sys_F_BouncingBall - (function () {
+const FactoryNewBall - (function () {
  //place entire script here:
   class BouncingBallClass {
     constructor (name, size, position_x, position_y, velocity_x, velocity_y) {
